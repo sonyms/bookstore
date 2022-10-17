db8c31d72c35bc05faa52325e8ba5f54e225742b
Sony MS <sonyms@gmail.com>
2022-10-17 20:08:45 +0000

    Update 3 files

--- pages/api/frontend/utils.js
+++ pages/api/frontend/utils.js
@@ -1,68 +1,77 @@
-import axios from 'axios';
+import axios from "axios";
+
+const url = process.env.API_URL;
+
 export const getAllBooks = async () => {
+  console.log( process.env.API_URL+"/api/books")
+  const response = await axios.get(
+    process.env.API_URL+"/api/books"
+  );
 
-    const response = await axios.get("http://localhost:3000/api/books");
-    
-    if(response.status !== 200) {
-        return new Error("Internal Server Error");
-    }
+  if (response.status !== 200) {
+    return new Error("Internal Server Error");
+  }
 
-    const data = await response.data;
+  const data = await response.data;
 
-    return data;
-
-}
+  return data;
+};
 
 export const getFeaturedBooks = async () => {
-    
-        const books = await getAllBooks();
-       
+  const books = await getAllBooks();
 
-        if(books!=undefined && books.length > 0) {
-
-            return books.filter(book => book.featured === true);
-            
-        }
-        else{
-           
-            return [];
-        }  
-    
-}   
+  if (books != undefined && books.length > 0) {
+    return books.filter((book) => book.featured === true);
+  } else {
+    return [];
+  }
+};
 
 export const saveBook = async (data) => {
-    try{
-        const res = await axios.post("http://localhost:3000/api/books", data);
-        console.log(res);
-        return await res.data;
-    }catch(err){
-        console.log(err);
-    }
-}
+  try {
+    const res = await axios.post(
+      "http://localhost:3001/api/books",
+      data
+    );
+    console.log(res);
+    return await res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const getBookWithId = async (id) => {
-    try{
-        const res = await axios.get(`http://localhost:3000/api/book/${id}`);
-        return await res.data;
-    }catch(err){
-        console.log(err);
-    }
-} 
+  try {
+   
+    console.log(process.env.API_URL+"/api/book/"+id)
+    const res = await axios.get(
+      "http://localhost:3001/api/book/"+id
+    );
+    return await res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const updateBook = async (id, data) => {
-    try{
-        const res = await axios.put(`http://localhost:3000/api/book/${id}`, data);
-        return await res.data;
-    }catch(err){
-        console.log(err);
-    }
-}
+  try {
+    const res = await axios.put(
+      "http://localhost:3001/api/book/"+id,
+      data
+    );
+    return await res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const deleteBook = async (id) => {
-    try{
-        const res = await axios.delete(`http://localhost:3000/api/book/${id}`);
-        return await res.data;
-    }catch(err){
-        console.log(err);
-    }
-}
+  try {
+    const res = await axios.delete(
+      "http://localhost:3001/api/book/"+id
+    );
+    return await res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
